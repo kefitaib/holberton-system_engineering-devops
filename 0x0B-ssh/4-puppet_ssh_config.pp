a38f029e80999fafd1d115ff6d113b42622e6fa0
kefitaib <taibkefi@yahoo.fr>
2020-08-04 13:14:34 -0700

    Puppet to make changes to our configuration file

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -5,3 +5,9 @@ file_line { 'add new config':
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton',
 }
+
+file_line { 'update config':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    replace => 'PasswordAuthentication no'
+}
