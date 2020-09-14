45c5aebd598e8d56e59e08c67cf2b8c1c2cdfef8
kefitaib <taibkefi@yahoo.fr>
2020-09-14 08:37:22 -0700

    extend your Python script to export data in the CSV format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    extend your Python script to export data in the CSV format
+
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    v2 = {'userId': argv[1]}
+    v = {'id': argv[1]}
+    s = ""
+
+    req1 = (requests.get('https://jsonplaceholder.typicode.com/users',
+                         params=v)).json()
+    req = (requests.get('https://jsonplaceholder.typicode.com/todos',
+                        params=v2)).json()
+
+    for r in req:
+        s += '"{}","{}","{}","{}"\n'.format(argv[1], req1[0].get('username'),
+                                            r.get('completed'), r.get('title'))
+
+    with open('2.csv', "w") as f:
+        f.write(s)
