4e683159b1ad75ee02456a4ce81c419c0752dd6b
kefitaib <taibkefi@yahoo.fr>
2020-09-17 13:45:05 -0700

    use Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -24,27 +24,25 @@ Chrome/39.0.2171.95 Safari/537.36'}
         word_dict = {k: 0 for k in word_list}
 
     if res.status_code == 404:
-        print()
+        return None
 
-    else:
-        for sub in res.json().get("data").get("children"):
-            l = [x for x in sub.get("data").get("title").split()]
-            for x in l:
-                for k, v in word_dict.items():
-                    if x.lower() == k.lower():
-                        word_dict[k] += 1
-                        continue
+    for sub in res.json().get("data").get("children"):
+        l = [x for x in sub.get("data").get("title").split()]
+        for x in l:
+            for k, v in word_dict.items():
+                if x.lower() == k.lower():
+                    word_dict[k] += 1
+                    continue
 
-        after = res.json().get("data").get("after")
-        if not after:
-            d = {k: v for k, v in sorted(word_dict.items(),
-                                         key=lambda item: item[1],
-                                         reverse=True)}
+    after = res.json().get("data").get("after")
+    if not after:
+        d = {k: v for k, v in sorted(word_dict.items(), key=lambda
+                                     item: item[1], reverse=True)}
 
-            for k, v in d.items():
-                if v != 0:
-                    print("{}: {}".format(k, v))
+        for k, v in d.items():
+            if v != 0:
+                print("{}: {}".format(k, v))
 
-            return
+        return
 
-        return count_words(subreddit, word_list, after, word_dict)
+    return count_words(subreddit, word_list, after, word_dict)
